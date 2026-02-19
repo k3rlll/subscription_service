ce59618f0edfdf7b60c668770e03882c6574523a
k3rlll <r_abdyashim@kbtu.kz>
2026-02-19 23:41:03 +0500

    feat: added logger setup

---
+++ pkg/logger/logger.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case "production":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case "development", "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	default:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
