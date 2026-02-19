b3ead44610ea29651f177f0aaed88474e6dc1749
k3rlll <r_abdyashim@kbtu.kz>
2026-02-19 23:57:12 +0500

    upd: updated custom request logger middleware

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -16,37 +16,39 @@ func MapRoutes(
 	// Middlewares
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
+	// Custom request logger middleware to log HTTP requests with structured logging
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		Skipper:   e.DefaultSkipper,
-		LogURI:    true,
-		LogMethod: true,
-		LogStatus: true,
-		LogError:  true,
+		LogURI:     true,
+		LogMethod:  true,
+		LogStatus:  true,
+		LogError:   true,
+		LogLatency: true, // duration of request processing for monitoring performance
 		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
 
-			if v.Error != nil {
-				logger.Error("HTTP request error",
-					"method", v.Method,
-					"uri", v.URI,
-					"status", v.Status,
-					"error", v.Error,
-				)
-				return nil
-			}
-
-			logger.Info("HTTP request",
+			// gather all log fields in a structured way
+			args := []any{
 				"method", v.Method,
 				"uri", v.URI,
 				"status", v.Status,
-				"error", v.Error,
-			)
+				"latency", v.Latency.String(),
+			}
+
+			if v.Error != nil {
+				args = append(args, "error", v.Error.Error())
+			}
+
+			// 500 and above are server errors, which we log as ERROR level
+			if v.Status >= 500 {
+				logger.Error("HTTP server error", args...)
+			} else if v.Status >= 400 {
+				// 400-499 are client errors, which we log as WARN level
+				logger.Warn("HTTP client error", args...)
+			} else {
+				// 200-399 are successful responses, which we log as INFO level
+				logger.Info("HTTP request success", args...)
+			}
 
 			return nil
 		},
-	},
-	))
-
-	//routes
-
-	logger.Info("HTTP routes mapped successfully")
+	}))
 }
